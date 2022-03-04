e6383960f4ea7cf429c97458fa84feaeb2f3862f
Josh Yap <joshua.c.yap@gmail.com>
2022-03-03 19:21:52 -0600

    tweaks

--- test.js
+++ test.js
@@ -1,4 +1,21 @@
+const bing = document.getElementById('bing');
+const duck = document.getElementById('duck')
+const google = document.getElementById('google')
+
 const compareResults = () => {
     let searchTerm = document.getElementById('searchTerm').value;
     console.log(searchTerm);
+    
+    bing.setAttribute("src", "./src/bing.png");
+    bing.setAttribute("height", "960");
+    bing.setAttribute("width", "590");
+
+    duck.setAttribute("src", "./src/duck.png");
+    duck.setAttribute("height", "960");
+    duck.setAttribute("width", "590");
+
+    google.setAttribute("src", "./src/google.png");
+    google.setAttribute("height", "960");
+    google.setAttribute("width", "590");
+    
 }
